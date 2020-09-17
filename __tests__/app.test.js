b230fd84371f7bfd3166bd05e83907ed5169b61e
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-09-17 14:36:29 -0700

    adds some worker tools

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,22 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const tweets = require('../lib/pipeline/producer');
+const { tweetData, store } = require('../lib/pipeline/tweet-worker');
 
 describe('newz-flash routes', () => {
+  console.log(tweets);
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+  });
+
+  it('tweet-worker should do it\'s job', async() => {
+    await tweetData(tweets);
+
+    await store();
+
+    const query = pool.query('SELECT * FROM tweets');
+
+    expect(query).toEqual('something');
   });
 });
