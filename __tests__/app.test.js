a688511fe79be9555e5bb621ba99cdda59fee092
Jerud-Moyer <jerudmoyer@gmail.com>
2020-09-17 18:12:10 -0700

    wowsers

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,19 +3,25 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 const tweets = require('../lib/pipeline/producer');
+const Email = require('../lib/models/emails');
 //const { tweetData, store } = require('../lib/pipeline/tweet-worker');
 
 describe('newz-flash routes', () => {
   console.log(tweets);
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 
-  it('tweet-worker should do it\'s job', async() => {
-    await tweetData(tweets);
+  
 
-    const query = pool.query('SELECT * FROM tweets');
+  it('should insert emails into the db', async() => {
+    const email = await Email.insert({
+      email: 'made up email'
+    });
+    const { rows } = await pool.query('SELECT * FROM emails WHERE ID = $1', [email.id]);
 
-    expect(query).toEqual('something');
+    expect(rows[0]).not.toBeUndefined();
+
   });
+
 });
